7e58e55e5c58da102dd0212389445a28ef2fad04
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-02-01 19:37:12 -0800

    added background image to cover html

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// var snowApi = 'https://pro.openweathermap.org/data/2.5/forecast/climate?q=London&appid=3fa1df04242d2493b1bb11f33d5c133f';
+// fetch(snowApi)
+//     .then(response => response.json())
+//     .then(data => { console.log(data)});
