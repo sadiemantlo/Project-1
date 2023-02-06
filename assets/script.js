84c86de71239536868d6657748ab7ccb08b5bc2f
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-02-06 13:10:36 -0800

    commit

--- assets/script.js
+++ assets/script.js
@@ -6,45 +6,38 @@ var ing3El = document.getElementById('ing3');
 var instructionsEl = document.getElementById('instructions');
 var imgEl = document.getElementById('img');
 var saveBttn = document.getElementById('save-bttn');
+var list = document.getElementById('local-storage');
 
-searchBttn.addEventListener('click', function() {
-    var getRecipe = 'https://thecocktaildb.com/api/json/v1/1/random.php';
-        fetch(getRecipe)
-            .then(response => response.json())
-            .then(data => { 
-                console.log(data);
-                var drinkName = data.drinks[0].strDrink;
-                var ing1 = data.drinks[0].strIngredient1;
-                var ing2 = data.drinks[0].strIngredient2;
-                var ing3 = data.drinks[0].strIngredient3;
-                var instructions = data.drinks[0].strInstructions;
-                var img = data.drinks[0].strDrinkThumb;
-                var measure1 = data.drinks[0].strMeasure1;
-                var measure2 = data.drinks[0].strMeasure2;
-                var measure3 = data.drinks[0].strMeasure3;
-                drinkNameEl.innerText = drinkName;
-                ing1El.innerText = measure1 + ing1;
-                ing2El.innerText = measure2 + ing2;
-                ing3El.innerText = measure3 + ing3;
-                instructionsEl.innerText = instructions;
-                imgEl.src = img;
-                 });
+searchBttn.addEventListener('click', function () {
+  var getRecipe = 'https://thecocktaildb.com/api/json/v1/1/random.php';
+  fetch(getRecipe)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      var drinkName = data.drinks[0].strDrink;
+      var ing1 = data.drinks[0].strIngredient1;
+      var ing2 = data.drinks[0].strIngredient2;
+      var ing3 = data.drinks[0].strIngredient3;
+      var instructions = data.drinks[0].strInstructions;
+      var img = data.drinks[0].strDrinkThumb;
+      var measure1 = data.drinks[0].strMeasure1;
+      var measure2 = data.drinks[0].strMeasure2;
+      var measure3 = data.drinks[0].strMeasure3;
+      drinkNameEl.innerText = drinkName;
+      ing1El.innerText = measure1 + ing1;
+      ing2El.innerText = measure2 + ing2;
+      ing3El.innerText = measure3 + ing3;
+      instructionsEl.innerText = instructions;
+      imgEl.src = img;
+    })
 
-})
+});
 
-// $(function () {
-//     // event listner for save buttons and saving to local storage
-//     $('#saveBtn').click(function() {
-//       var blockEl = $(this).parent();
-//       var savedDrinks = blockEl.children(h1).val();
-//       localStorage.setItem('drink-name', savedDrinks);
-//     })
 
-//   });
-
-  saveBttn.addEventListener('click', function() {
-    console.log('clicked');
-    var savedDrinks = drinkNameEl.textContent;
-    console.log(savedDrinks);
-    localStorage.setItem('drink-name', savedDrinks)
-  })
+function save() {
+  console.log('clicked');
+  var savedDrinks = drinkNameEl.textContent;
+  console.log(savedDrinks);
+  localStorage.setItem('drink-name', savedDrinks)
+}
+saveBttn.addEventListener('click', save);
